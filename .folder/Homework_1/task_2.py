7ecda2068078f9778b332c911f5b5b0e6097d71f
Evgeniya <loiseau87bleu@gmail.com>
2023-04-22 11:22:04 +0400

    task2

--- .folder/Homework_1/task_2.py
+++ .folder/Homework_1/task_2.py
@@ -5,6 +5,19 @@
 123 -> 6 (1 + 2 + 3)
 100 -> 1 (1 + 0 + 0) 
 """
-i = input()
+#Решение 1
+#i = input()
 
-print(int(i[0])+int(i[1])+int(i[2]))
+#print(int(i[0])+int(i[1])+int(i[2]))
+
+#Решение 2
+num = int(input("Введите трехзначное число: "))
+
+# Получение цифр числа
+digit1 = num // 100
+digit2 = (num % 100) // 10
+digit3 = num % 10
+
+# Вычисление суммы цифр
+sum_of_digits = digit1 + digit2 + digit3
+print(f"Сумма цифр числа {num} равна {sum_of_digits}")
