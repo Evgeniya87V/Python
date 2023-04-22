15466845997de06b37b14f9dacb8cc9630f6b0e4
Evgeniya <loiseau87bleu@gmail.com>
2023-04-22 12:20:18 +0400

    task10

---
+++ .folder/Homework_2/task_10.py
@@ -0,0 +1,42 @@
+"""
+Задача 10: 
+На столе лежат n монеток. Некоторые из них лежат вверх решкой, 
+а некоторые – гербом. Определите минимальное число монеток, которые нужно перевернуть, 
+чтобы все монетки были повернуты вверх одной и той же стороной.
+Выведите минимальное количество монет, которые нужно перевернуть
+"""
+
+from random import randint
+countN = int(input("Введите число монеток: "))
+tails = 0
+eagle = 0
+count = 0
+for i in range(countN):
+    x = randint(0, 1)
+    if x == 0:
+        eagle += 1
+    else:
+        tails += 1
+    print(x)
+if eagle >= tails:
+    count = countN - eagle
+else:
+    count = countN - tails
+print('Минимальное количество монет, которые нужно перевернуть:', count)
+
+#решение 2 
+"""
+n = int(input())
+count_zero = 0
+count_one = 0
+for i in range(n):
+    x = int(input())
+    if x == 0:
+        count_zero += 1
+    else:
+        count_one += 1
+if count_one > count_zero:
+    print(count_zero)
+else:
+    print(count_one)
+"""
